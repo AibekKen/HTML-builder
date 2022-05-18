4f5e00230d0e0447ddc4f6d2b402623e8983b457
Aibek <ai.bek@mail.ru>
2022-05-18 13:12:47 +0600

    feat: done task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const writeBundle = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+const stylesDir = path.join(__dirname, 'styles');
+
+fs.readdir(stylesDir, (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    if (path.extname(file) === '.css') {
+      const readStyle = fs.createReadStream(path.join(stylesDir, file), 'utf-8');
+      readStyle.on('data', chunk => writeBundle.write(chunk));
+    }
+  });
+});
