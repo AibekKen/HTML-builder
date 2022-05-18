96046b2b50a45b393f90980756a9b8f60b9ca837
Aibek <ai.bek@mail.ru>
2022-05-18 11:57:08 +0600

    feat: done task 04-copy-dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+const filesCopy = path.join(__dirname, 'files-copy');
+const files = path.join(__dirname, 'files');
+
+async function copyDir() {
+  await fsPromises.mkdir(filesCopy, { recursive: true });
+  fs.readdir(filesCopy, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.unlink(path.join(filesCopy, file), err => {
+        if (err) throw err;
+      });
+    });
+  });
+
+  fs.readdir(files, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach((dirent) => {
+      if (dirent.isFile()) {
+        fsPromises.copyFile(path.join(__dirname, 'files', dirent.name), path.join(filesCopy, dirent.name));
+      }
+    });
+  });
+}
+
+copyDir();
+
 
